312b1c0b03438a339a8f037a49d93e23ddde19bd
jonathan <jonathan.lemari@gmail.com>
2025-05-10 17:47:03 +0200

    V2 Edit comment. comment editing directly into the comment where Object in page is. User can only edit once. Next: Ajax feature

--- comment/views.py
+++ comment/views.py
@@ -22,7 +22,7 @@ def delete_comment(request, pk):
                 comment.delete()
                 print(f"The comment has been deleted: {comment}")
                 messages.success(request, "Comment deleted successfully")
-                return redirect(request.META.get('HTTP_REFERER')) # , 'movie:movie_overview'
+                return redirect(request.META.get('HTTP_REFERER'))
             else:
                 messages.error(request, "Error deleting comment, Comment does not exist or you are not the owner")
                 return redirect("main:home")
@@ -50,28 +50,41 @@ def edit_comment(request, pk):
         print(f"\n* Unauthorised acces: User {request.user} tried to access the editing  Comment belonging to another user *\n")
         messages.error(request, ("You are not authorized to acces this page."))
         return redirect(to='main:home')
+    
+    # if user somehow tries to edit a comment that was already edited, redirect to the detail page of the movie or serie that the comment belongs to
+    elif request.user.is_authenticated and comment.user == request.user and comment.created_at != comment.updated_at:
+        print(f"\n* Unauthorised acces: User {request.user} tried to edit a  Comment more than once. Forbidden *\n")
+        messages.error(request, ("You are not authorized to acces this page. This comment was already edited."))
+        if comment.content_type == "movie":
+            movie = Movie.objects.get(pk=comment.object_id)
+            slug = movie.slug
+            return redirect('movie:detail', slug=slug)
+        elif comment.content_type == "serie":
+            serie = Serie.objects.get(pk=comment.object_id)
+            slug = serie.slug
+            return redirect('serie:detail', slug=slug)
 
-    elif request.user.is_authenticated and comment.user == request.user:
+
+    elif request.user.is_authenticated and comment.user == request.user and comment.created_at == comment.updated_at:
         try:
             print(f"comment to edit: {comment}")
+            # create a comment form  with the existing comment body instance
             form = CommentForm(request.POST or None, instance=comment)
+            # if request.method == "GET":
 
-            if request.method == "GET":
-                print(f" User: {request.user.username} trying to edit a comment!")
-                # form = CommentForm(instance=comment)
-                context = {
-                    'form': form,
-                    'comment': comment,
-                }
-                return render(request, 'comment/edit.html', context)
+            #     print(f" User: {request.user.username} got to the Edit Comment page!")
+            #     # form = CommentForm(instance=comment)
+            #     context = {
+            #         'form': form,
+            #         'comment': comment,
+            #     }
+            #     return render(request, 'comment/edit.html', context)
 
             if request.method == "POST":
                 print(f" User: {request.user.username} editing a comment!")
                 if form.is_valid():
+                    print("is form valid or not ?")
                     form.save(commit=False)
-                    # form.instance.user = request.user
-                    # form.instance.content_type = "movie"
-                    # form.instance.object_id = movie.pk
                     form.instance.body = form.cleaned_data['body']
                     form.save(commit=True)
                     print(f"The comment has been edited: {comment}")
@@ -84,31 +97,21 @@ def edit_comment(request, pk):
                         serie = Serie.objects.get(pk=comment.object_id)
                         slug = serie.slug
                         return redirect('serie:detail', slug=slug)
+                    else:
+                        # in case of error/form invalid
+                        messages.error(request, "Invalid comment form submitted.")
+                        return redirect(request.META.get('HTTP_REFERER'))
 
 
-
-            # context = {
-            #     'form': form,
-            #     'comment': comment,
-            # }
-            # return render(request, 'comment/edit.html', context)
-
-
-        
-                # if the form is valid, save the comment and redirect to the movie page
-
-
-
-        #         # comment.delete()
-        #         print(f"The comment has been edited: {comment}")
-        #         messages.success(request, "Comment edited successfully")
-        #         return redirect(request.META.get('HTTP_REFERER'))
-        #     else:
-        #         messages.error(request, "Error editing the comment, Comment does not exist or you are not the owner")
-        #         return redirect("main:home")
-        
         except Exception as e:
             print(traceback.format_exc())
             print(f"Error in trying to edit a comment\n Error: {e}")
-            messages.error(request, "The comment does not exist or you are not the owner")
-            return redirect("main:home")
+            messages.error(request, "The comment either does not exist, you are not the owner or an internal error occurred")
+            if comment.content_type == "movie":
+                movie = Movie.objects.get(pk=comment.object_id)
+                slug = movie.slug
+                return redirect('movie:detail', slug=slug)
+            elif comment.content_type == "serie":
+                serie = Serie.objects.get(pk=comment.object_id)
+                slug = serie.slug
+                return redirect('serie:detail', slug=slug)
