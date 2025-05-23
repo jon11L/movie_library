168226a4014d987f688aecec797d7e49bcbeecc3
jonathan <jonathan.lemari@gmail.com>
2025-05-23 13:41:02 +0200

    fixed bug on comment editing. mo multiple (edited) showing

--- static/js/edit_comment.js
+++ static/js/edit_comment.js
@@ -3,8 +3,6 @@ function editComment() {
     // and display the edited comment
     console.log('edit comment feature loaded');
 
-    const editButtons = document.querySelectorAll('.edit-comment-form-btn');
-
     function closeAllEdits() {
         // Prevent from opening several edit comment form, Ensure one Edit form at a time 
         document.querySelectorAll('.edit-comment-block').forEach(div => div.style.display = 'none');
@@ -23,14 +21,26 @@ function editComment() {
             `
         );
         messageContainer.fadeIn();
-        
         // Hide message after 3,5 seconds
         setTimeout(() => messageContainer.fadeOut(), 3500);
     }
-
+    
+    const editButtons = document.querySelectorAll('.edit-comment-form-btn');
     // Click on edit button to trigger the edit form
-    editButtons.forEach(button => {
-        button.addEventListener('click', function (){
+    editButtons.forEach(editBtn => {
+
+        // This is to prevent editbutton being reattach with an handler when a new comment is posted
+        // Ensuring Only once the edit button is triggered // same for submit
+        if (editBtn.dataset.bound === 'true') {
+            console.log("the button is already bound.");
+            return;
+        } else {
+            editBtn.dataset.bound = 'true';
+            console.log("Adding the bound on editbutton.");
+        }
+
+
+        editBtn.addEventListener('click', function (){
             //  display the new edit form with the current text comment(body) 
             // and hide plain text
 
@@ -44,31 +54,20 @@ function editComment() {
             // fetch the comment.id
             const commentId = this.dataset.commentId;
             const originalBody = this.dataset.commentBody;
+
+            // Get the whole comment block from where the edit-btn was clicked
             const commentBlock = document.getElementById(`comment-${commentId}`);
-            // console.log(`comment id: ${commentId}`);
+
             console.log(`edit feature for comment ${commentId} opened.`)
             console.log(`original body-text: ${originalBody}`);
 
-            //  elements that will be modified on the page, 
-            // remove the edit button and add (edited) on the instance block
             
             const createdAt = commentBlock.querySelector('.created-at')
-            const selectEdit = commentBlock.querySelector('.edit-comment-button')
-            if (selectEdit) {
-                console.log('the edit trigger button was selected')
-            }
 
             // Remove any other Edit and delete button while on editing mode.
             closeAllEdits();
-            
-            // Check if the commentBlock exists
-            // if (!commentBlock) {
-            //     console.error(`No comment block found for ID comment-${commentId}`);
-            //     return;
-            // } else {
-            //     console.log(`comment block found for ID comment-${commentId}`);
-            // }
-            
+
+            //  Show edit form and hide original text & buttons 
             commentBlock.querySelector('.comment-body').style.display = 'none';
             document.querySelectorAll('.comment-buttons').forEach(div => div.style.display = 'none');
             commentBlock.querySelector('.edit-comment-block').style.display = 'block';
@@ -78,92 +77,103 @@ function editComment() {
             textarea.value = originalBody;
             
             // Cancel button on edit comments / return to origin DOM.
-            const cancelButtons = document.querySelectorAll('.cancel-btn');
-            cancelButtons.forEach(button => {
-                button.addEventListener('click', function() {
+            const cancelButton = commentBlock.querySelector('.cancel-btn');
+            // cancelButtons.forEach(button => {
+                // cancelButton.addEventListener('click', function() {
+                cancelButton.onclick = function() {
+                // button.addEventListener('click', function() {
                     closeAllEdits();
                     textarea.value = originalBody;
-                    console.log('User canceled the comment editing.')
-                });
-            });
 
-            // Submit button once edit comments form is opened.
+                    // if (editBtn.dataset.bound === 'true') {
+                    //     console.log("editbutton Bound needs to be removed");
+                    //     editBtn.dataset.bound = 'false';
+                    //     console.log("removing the bound");
+                    // }
+
+                    console.log('User canceled the comment editing.');
+                };
+            // });
+
+
             const submitButton = commentBlock.querySelector('button[type="submit"]');
-            submitButton.addEventListener('click', function(e) {
-                console.log('submit button clicked!!!!!');
-                e.preventDefault(); // preventDefault allow the page to not reload on execution
+            // Check if submitButton was already having and listener handler. 
+            // if so then pass, to avoid multiple handlers being attached to.
+            if (submitButton.dataset.bound !== 'true') {
+                submitButton.dataset.bound = 'true';
 
+                // Submit button once edit comments form is opened.
+                submitButton.addEventListener('click', function(e) {
+                    console.log('submit button clicked!!!!!');
+                    e.preventDefault(); // preventDefault allow the page to not reload on execution
 
-                // fetch the info from the data attribute in the button
-                const contentType = submitButton.dataset.contentType;  // 'movie' or 'serie'
-                const objectId = submitButton.dataset.objectId; // Id of the object.
-                // console.log(`Submit Button contains the following: ${formData}`);
-                console.log(`content type: ${contentType}`);
-                console.log(`object id: ${objectId}`);
-                console.log(`the comment id: ${commentId}`);
+                    // fetch the info from the data attribute in the button
+                    const contentType = submitButton.dataset.contentType;  // 'movie' or 'serie'
+                    const objectId = submitButton.dataset.objectId; // Id of the object.
+                    // console.log(`Submit Button contains the following: ${formData}`);
+                    console.log(`content type: ${contentType}`);
+                    console.log(`object id: ${objectId}`);
+                    console.log(`the comment id: ${commentId}`);
 
-                // get the new edited form.body that was passed from html
-                const new_body = textarea.value;
-                console.log(`form body: ${new_body}`);
+                    // get the new edited form.body that was passed from html
+                    const new_body = textarea.value;
+                    console.log(`form body: ${new_body}`);
 
-                // const editCommentForm = document.getElementById('edit-comment-form');
-                // send the data to the server via Fetch API
-                fetch(`/comment/edit/${commentId}/`, {
-                    method: 'PUT',
-                    headers: {
-                    'X-CSRFToken': getCSRFToken() // CSRF for Django
-                    },
-                    // need to figure out what to pass here..
-                    body: JSON.stringify({body: new_body})
-                }) 
+                    // send the data to the server via Fetch API
+                    fetch(`/comment/edit/${commentId}/`, {
+                        method: 'PUT',
+                        headers: {
+                        'X-CSRFToken': getCSRFToken() // CSRF for Django
+                        },
+                        // need to figure out what to pass here..
+                        body: JSON.stringify({body: new_body})
+                    }) 
 
-                .then(response => {
-                    if (!response.ok) {  // If response is error (e.g., 400, 500), throw error
-                        throw new Error(`Network response was not ok. status: ${response.status}`);
-                    }
-                    console.log('response ok');
-                    console.log(`response: ${response}`);
-                    return response.json();  // Parse response JSON
-                })
-                //  if return is successful then update the DOM. 
-                .then(data => {
-                    if (data.success) {
-                        // Update the comment body in the DOM
-                        const commentBlock = document.getElementById(`comment-${commentId}`);
-                        if (commentBlock) {
-                            
-                            const commentBody = commentBlock.querySelector('.comment-body');
-                            commentBody.innerHTML = `<p>${new_body}</p>`; // Update the comment body
-                            closeAllEdits();
-
-                            // hide the edit button and display'edited'
-                            selectEdit.style.display = "none";
-                            createdAt.insertAdjacentHTML("afterbegin" ,`
-                            <p style="margin-right: 8px;">(Edited) </p>
-                            `);
-
-                            console.log(`Comment ${commentId} edited successfully.`);
-                            showMessage(data.message, 'success');
-                        } else {
-                            console.error(`No comment block found for ID comment-${commentId}`);
-                            showMessage("Edit Failed...", "danger")
+                    .then(response => {
+                        if (!response.ok) {  // If response is error (e.g., 400, 500), throw error
+                            throw new Error(`Network response was not ok. status: ${response.status}`);
                         }
-                    }
+                        console.log('response ok');
+                        console.log(`response: ${response}`);
+                        return response.json();  // Parse response JSON
+                    })
+                    //  if return is successful then update the DOM. 
+                    .then(data => {
+                        if (data.success) {
+                            // Update the comment body in the DOM
+                            if (commentBlock) {
+                                const commentBody = commentBlock.querySelector('.comment-body');
+                                commentBody.innerHTML = `<p>${new_body}</p>`; // Update the comment body
+                                closeAllEdits();
+
+                                // hide the edit button and display'edited'
+                                // selectEdit.style.display = "none";
+                                editBtn.style.display = "none"; // hide the current edit button that was triggered
+
+                                createdAt.insertAdjacentHTML("afterbegin" ,`
+                                <p style="margin-right: 8px;">(Edited) </p>
+                                `);
+
+                                console.log(`Comment ${commentId} edited successfully.`);
+                                showMessage(data.message, 'success');
+                            } else {
+                                console.error(`No comment block found for ID comment-${commentId}`);
+                                showMessage("Edit Failed...", "danger")
+                            }
+                        }
+                    })
+
+                .catch(error => {
+                    console.error('Error', error);
+                    // alert('Failed to edit the comment, please reload the page');
+                    showMessage('Failed to edit the comment, please reload the page', 'danger')
                 })
 
-            .catch(error => {
-                console.error('Error', error);
-                // alert('Failed to edit the comment, please reload the page');
-                showMessage('Failed to edit the comment, please reload the page', 'danger')
-            })
-
-            });
+                });
+            }
         });
         
     });
 }
-
-
-
 
 document.addEventListener("DOMContentLoaded", editComment);
