2c6d0e641fbb9233f0d6049c4ecc971d493c4174
jon11L <jonathan.lemari@gmail.com>
2025-09-19 01:05:22 +0200

    (Watchlist serializer) added custom permission, Staff & user owner of the object can Add/update watchlist

--- user_library/serializers.py
+++ user_library/serializers.py
@@ -1,25 +1,35 @@
 from rest_framework import serializers
 
 from .models import WatchList
+from movie.models import Movie
+from serie.models import Serie
 
-from movie.serializers import MovieListSerializer, MovieDetailSerializer
-from serie.serializers import SerieListSerializer, SerieDetailSerializer
+from movie.serializers import MovieListSerializer
+from serie.serializers import SerieListSerializer
 
-
-
-# Serializer for the Movie model:
-# serialize the movie model and sanitize the data, and create api endpoints so that other apps can use it
-# This serializer will be used to convert the Movie model instances into JSON format and vice versa
-# It will also handle validation and serialization of the data.
-# This is a basic serializer that will includes most fields of the Movie model
-
-# user or externals apps should be able to use this serializer to retrieve movies. by title and genre
-#  limited to read-only access to regular user and POST/PUT/UPDATE for ADMIN/Superuser/staff
 
 class WatchListSerializer(serializers.ModelSerializer):
 
+    # Writable fields for POST operation
+    movie_id = serializers.PrimaryKeyRelatedField(
+        queryset=Movie.objects.all(),
+        source="movie",
+        write_only=True,
+        required=False,
+        allow_null=True,
+    )
+    serie_id = serializers.PrimaryKeyRelatedField(
+        queryset=Serie.objects.all(),
+        source="serie",
+        write_only=True,
+        required=False,
+        allow_null=True,
+    )
+
+    # to display the Movie/serie instance within the Watchlist object's instance
     movie = MovieListSerializer(read_only=True)
     serie = SerieListSerializer(read_only=True)
+
 
     class Meta:
         model = WatchList
@@ -27,14 +37,19 @@ class WatchListSerializer(serializers.ModelSerializer):
             'id',
             'user',
             'movie',
+            'movie_id',
             'serie',
+            'serie_id',
             'status',
             'personal_note',
         ]
+        read_only_fields = ['user']
+
 
     def to_representation(self, instance):
         '''
-        Clean out Null data for Movie or Serie. As only one of is saved by instances
+        Remove Null data for Movie or Serie. As only one of is saved by instances\n
+        So only the actual media saved in watchlist shows.
         '''
         data = super().to_representation(instance)
 
@@ -42,19 +57,15 @@ class WatchListSerializer(serializers.ModelSerializer):
             data.pop('movie', None)
         if data['serie'] is None:
             data.pop('serie', None)
-            
-        return data
-    
 
-    
-# class WatchListSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = WatchList
-#         fields = (
-#             'id',
-#             'user',
-#             'movie',
-#             'serie',
-#             'status',
-#             'personal_note',
-#         )        
+        return data
+
+
+    def validate(self, data):
+        '''Only 1 Movie or 1 serie to be saved at a time, one must remain Null'''
+        if not data.get('movie') and not data.get('serie'):
+            raise serializers.ValidationError("You must provide either a movie or a serie.")
+        if data.get('movie') and data.get('serie'):
+            raise serializers.ValidationError("You cannot provide both movie and serie, only one.")
+
+        return data
