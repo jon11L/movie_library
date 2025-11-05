d06445119764bae4ffcb002f8d868147c849a335
jon11L <jonathan.lemari@gmail.com>
2025-11-06 00:01:28 +0100

    Testing Movie model fields & methods ongoing.

--- movie/tests/test_models.py
+++ movie/tests/test_models.py
@@ -46,7 +46,6 @@ class MovieModelSaveTest(TestCase):
     def test_fields_saved_correctly(self):
         """Test that all fields are saved and retrieved correctly."""
         movie = self.movie  # Using the movie created in setUpTestData
-        self.assertEqual(movie.tagline, "Mischief. Mayhem. Soap.")
         self.assertEqual(movie.director, ["David Fincher"])
         self.assertEqual(movie.writer, ["Chuck Palahniuk", "Jim Uhls"])
         self.assertEqual(movie.production, ["20th Century Fox"])
@@ -56,11 +55,11 @@ class MovieModelSaveTest(TestCase):
         self.assertEqual(movie.status, "Released")
         self.assertEqual(movie.budget, 63000000)
         self.assertEqual(movie.revenue, 100853753)
-        self.assertEqual(movie.vote_average, 8.4)
         self.assertEqual(movie.vote_count, 12000)
         self.assertEqual(movie.popularity, 45.0)
         self.assertEqual(movie.poster_images, ["poster1.jpg", "poster2.jpg"])
         self.assertEqual(movie.banner_images, ["banner1.jpg", "banner2.jpg"])
+
 
     def test_movie_str_method(self):
         """Test the __str__ method of the Movie model."""
@@ -71,6 +70,12 @@ class MovieModelSaveTest(TestCase):
         self.assertNotEqual(self.movie.title, "Inception")
         self.assertEqual(self.movie.title, "Fight Club")
         self.assertEqual(type(self.movie.title), str)
+
+    def test_tagline_field(self):
+        """Test that the tagline field is saved correctly."""
+        movie = self.movie
+        self.assertEqual(type(movie.tagline), str)
+        self.assertEqual(movie.tagline, "Mischief. Mayhem. Soap.")
 
     def test_description_field(self):
         """Test that the description field is saved correctly."""
@@ -85,7 +90,6 @@ class MovieModelSaveTest(TestCase):
         self.assertIsInstance(self.movie.genre, list)
         self.assertIsNotNone(self.movie.genre)
         self.assertEqual(self.movie.genre, ["Drama", "Thriller"])
-
         if self.movie.genre:
             self.assertIn("Drama", self.movie.genre)
             self.assertIn("Thriller", self.movie.genre)
@@ -95,6 +99,15 @@ class MovieModelSaveTest(TestCase):
         """Test that a movie can be marked as released."""
         self.assertEqual(type(self.movie.released), bool)
         self.assertTrue(self.movie.released)
+
+
+    def test_serie_status_field(self):   
+        """Test that a movie can be marked as released."""
+        # movie = Movie.objects.get(pk=1)
+        self.assertEqual(type(self.movie.status), str)
+        # self.assertTrue(self.serie.status)
+        self.assertIsNotNone(self.movie.status)
+        self.assertTrue(self.movie.status == "Released")
 
     def test_movie_length_field(self):
         """Test that the length field is saved correctly."""
@@ -121,6 +134,41 @@ class MovieModelSaveTest(TestCase):
             self.assertEqual(movie.casting[2]['name'], "Helena Bonham Carter")
             self.assertEqual(movie.casting[2]['role'], "Marla Singer")
 
+    def test_serie_production_field(self):
+        """Test that the production field can store a list of strings."""
+        movie = self.movie
+        self.assertIsInstance(movie.production, list)
+        self.assertIsNotNone(movie.production)
+        if movie.production:
+            self.assertEqual(len(movie.production), 1)
+            self.assertIn("20th Century Fox", movie.production)
+
+    def test_serie_original_language_field(self):
+        """Test that the original_language field is saved correctly."""
+        movie = self.movie
+        self.assertIsNotNone(movie.original_language)
+        self.assertEqual(type(movie.original_language), str)
+        if movie.original_language:
+            self.assertEqual(movie.original_language, "en")
+            self.assertIn("en", movie.original_language)
+    
+    def test_serie_spoken_languages_field(self):
+        """Test that the spoken_languages field can store a list of strings."""
+        movie = self.movie
+        self.assertIsNotNone(movie.spoken_languages)
+        self.assertIsInstance(movie.spoken_languages, list)
+        if movie.spoken_languages:
+            self.assertIn("en", movie.spoken_languages)
+            self.assertIn("de", movie.spoken_languages)
+            self.assertEqual(len(movie.spoken_languages), 2)
+
+    def test_vote_average_field(self):
+        """Test that the vote_average field is saved correctly."""
+        movie = self.movie
+        self.assertIsNotNone(movie.vote_average)
+        self.assertEqual(type(movie.vote_average), float)
+        self.assertEqual(movie.vote_average, 8.4)
+
     # may go with Core app (but here tied to Movie model at the moment)
     def test_slug_generated_on_save(self):
         """Test that a slug is generated upon saving the movie."""
@@ -136,10 +184,8 @@ class MovieModelSaveTest(TestCase):
             title="Inception",
             tmdb_id=27205
             )
-        
         self.assertEqual(movie.title, "Inception")
         self.assertIsNotNone(movie.pk)  # Has a database ID
-
 
     def test_movie_tmdb_id_unique_constraint(self):
         """
@@ -147,18 +193,8 @@ class MovieModelSaveTest(TestCase):
         Fail on attempting to create a new movie with an already existing tmdb_id.
         """
         Movie.objects.create(title="Movie 1", tmdb_id=12345)
-        
         with self.assertRaises(Exception):  # Should raise IntegrityError
             Movie.objects.create(title="Movie 2", tmdb_id=12345)
-
-
-    def test_movie_creation_missing_required_fields(self):
-        """Test creating a movie without required fields raises an error."""
-        with self.assertRaises(Exception):  # Should raise IntegrityError
-            Movie.objects.create(
-                tmdb_id=54321
-            )
-
 
     def test_movie_trailers_field(self):
         """Test that the trailers field is saved correctly."""
@@ -170,6 +206,8 @@ class MovieModelSaveTest(TestCase):
             self.assertEqual(type(movie.trailers[0]), dict)
             self.assertEqual(movie.trailers[0]['key'], "8hP9D6kZseM")
             self.assertEqual(movie.trailers[0]['website'], "youtube")
+
+
 
 class MovieModelMethodTest(TestCase):
 
@@ -187,7 +225,7 @@ class MovieModelMethodTest(TestCase):
             trailers=[{"key": "8hP9D6kZseM", "website": "youtube"}],
             released=True,
             origin_country=["US"],
-            vote_average=8.8,
+            vote_average=8.812,
             spoken_languages=["en", "fr"],
         )
 
@@ -200,41 +238,41 @@ class MovieModelMethodTest(TestCase):
             director=["Christopher Nolan"]
 
         )
+        
+        Movie.objects.create(
+            original_title="Fight Club",
+            title="Fight Club",
+            description="A ticking-time-bomb insomniac and a slippery soap salesman channel primal..",
+            tagline="Mischief. Mayhem. Soap.",
+            genre=["Drama", "Thriller"],
+            release_date=datetime.datetime.strptime("1999-10-15", '%Y-%m-%d').date(),
+            length=139,
+            released=True,
+            director=["David Fincher"],
+            writer=["Chuck Palahniuk", "Jim Uhls"],
+            production=["20th Century Fox"],
+            casting=[
+                {"name": "Brad Pitt", "role": "Tyler Durden"},
+                {"name": "Edward Norton", "role": "The Narrator"},
+                {"name": "Helena Bonham Carter", "role": "Marla Singer"},
+                {"name": "Meat Loaf", "role": "Robert 'Bob' Paulson"}
+                ],
+            origin_country=["US"],
+            original_language="en",
+            spoken_languages=["en", "de"],
+            status="Released",
+            budget=63000000,
+            revenue=100853753,
+            vote_average=8.4,
+            vote_count=1700000,
+            imdb_rating=8.8,
+            popularity=45.0,
+            poster_images=["poster1.jpg", "poster2.jpg"],
+            banner_images=["banner1.jpg", "banner2.jpg"],
+            trailers=[{"key": "8hP9D6kZseM", "website": "youtube"}]
+        )
 
         print("         --SetupTestData for MovieModelMethodTest completed.\n")
-        # Movie.objects.create(
-        #     original_title="Fight Club",
-        #     title="Fight Club",
-        #     description="A ticking-time-bomb insomniac and a slippery soap salesman channel primal..",
-        #     tagline="Mischief. Mayhem. Soap.",
-        #     genre=["Drama", "Thriller"],
-        #     release_date="1999-10-15",
-        #     length=139,
-        #     released=True,
-        #     director=["David Fincher"],
-        #     writer=["Chuck Palahniuk", "Jim Uhls"],
-        #     production=["20th Century Fox"],
-        #     casting=[
-        #         {"name": "Brad Pitt", "role": "Tyler Durden"},
-        #         {"name": "Edward Norton", "role": "The Narrator"},
-        #         {"name": "Helena Bonham Carter", "role": "Marla Singer"},
-        #         {"name": "Meat Loaf", "role": "Robert 'Bob' Paulson"}
-        #         ],
-        #     origin_country=["US"],
-        #     original_language="en",
-        #     spoken_languages=["en", "de"],
-        #     status="Released",
-        #     budget=63000000,
-        #     revenue=100853753,
-        #     vote_average=8.4,
-        #     vote_count=1700000,
-        #     imdb_rating=8.8,
-        #     popularity=45.0,
-        #     poster_images=["poster1.jpg", "poster2.jpg"],
-        #     banner_images=["banner1.jpg", "banner2.jpg"],
-        #     trailers=[{"key": "8hP9D6kZseM", "website": "youtube"}]
-        # )
-
 
     # def setUp(self):
     #     Movie.objects.create(
@@ -275,35 +313,44 @@ class MovieModelMethodTest(TestCase):
     def test_render_length(self):
         movie = Movie.objects.get(id=1)
         self.assertEqual(movie.render_length(), "2h28")
-
         movie_2 = Movie.objects.get(id=2)
         self.assertEqual(movie_2.render_length(), "N/a")
 
     def test_render_casting(self):
         movie = Movie.objects.get(id=1)
+        self.assertEqual(type(movie.render_casting()), str)
         self.assertEqual(movie.render_casting(), "Leonardo DiCaprio as Cobb || Joseph Gordon-Levitt as Arthur")
-
         movie_2 = Movie.objects.get(id=2)
-        self.assertEqual(type(movie_2.render_casting()), str)
         self.assertNotEqual(movie_2.render_casting(), "Leonardo DiCaprio as Cobb || Joseph Gordon-Levitt as Arthur")
         self.assertEqual(movie_2.render_casting(), "Christian Bale as Bruce Wayne || Heath Ledger as Joker")
 
     def test_render_writer(self):
         movie = Movie.objects.get(id=1)
+        self.assertNotEqual(movie.render_writer(), "Robbert eggers")
         self.assertEqual(movie.render_writer(), "Christopher Nolan")
 
     def test_render_production(self):
         movie = Movie.objects.get(id=1)
+        self.assertEqual(type(movie.render_production()), str)
+        self.assertNotEqual(movie.render_production(), "20th Century Fox")
         self.assertEqual(movie.render_production(), "Warner Bros.")
 
     def test_render_director(self):
         movie = Movie.objects.get(id=1)
+        self.assertEqual(type(movie.render_director()), str)
+        self.assertNotEqual(movie.render_director(), "Steven Spielberg")
         self.assertEqual(movie.render_director(), "Christopher Nolan")
 
-    # def test_render_trailers(self):
-    #     movie = Movie.objects.get(id=1)
-    #     # self.assertEqual(type(movie.render_trailer), str)
-    #     # self.assertIn("youtube", movie.render_trailer)
-    #     # self.assertIn("8hP9D6kZseM", movie.render_trailer())
-    #     self.assertEqual(movie.render_trailer(), "https://www.youtube.com/watch?v=8hP9D6kZseM")
+    def test_render_vote_average(self):
+        movie = Movie.objects.get(title="Fight Club")
+        self.assertNotEqual(movie.render_vote_average(), 6)
+        self.assertEqual(movie.render_vote_average(), 8.4)
+        movie = Movie.objects.get(title="Inception")
+        self.assertEqual(movie.render_vote_average(), 8.8)
 
+    def test_render_poster_images(self):
+            serie = Movie.objects.get(title="Fight Club")
+            serie.poster_images = ["poster1.jpg", "poster2.jpg"]
+            self.assertNotEqual(serie.render_poster(), "poster1.jpg")
+            self.assertEqual(serie.render_poster(), "https://image.tmdb.org/t/p/w500poster1.jpg")
+            self.assertNotEqual(serie.render_poster(), "poster2.jpg")
