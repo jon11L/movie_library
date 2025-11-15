5a9587b65d242f8637c578af5bd333b84256ea06
jon11L <jonathan.lemari@gmail.com>
2025-11-15 23:03:37 +0100

    Extended test Movie/Serie validations, empty fields constraints.

--- movie/tests/test_models.py
+++ movie/tests/test_models.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from django.db import IntegrityError
 from django.templatetags.static import static
+from django.core.exceptions import ValidationError
 
 import datetime
 
@@ -24,7 +26,6 @@ class MovieModelSaveTest(TestCase):
             original_language="en",
             spoken_languages=["en", "de"],
             length=139,
-            released=True,
             production=["20th Century Fox"],
             director=["David Fincher"],
             writer=["Chuck Palahniuk", "Jim Uhls"],
@@ -44,60 +45,65 @@ class MovieModelSaveTest(TestCase):
             banner_images=["banner1.jpg", "banner2.jpg"],
             trailers=[{"key": "8hP9D6kZseM", "website": "youtube"}],
         )
+        cls.movie_empty = Movie.objects.create(
+            tmdb_id=5150,
+            title="Test title",
+        )
 
-    def test_fields_saved_correctly(self):
+        print("\n\n--SetupTestData for MovieModelSaveTest completed.\n")
+
+
+    def test_fields_list_correctly(self):
         """Test that all fields are saved and retrieved correctly."""
-        movie = self.movie  # Using the movie created in setUpTestData
-        self.assertEqual(movie.director, ["David Fincher"])
-        self.assertEqual(movie.writer, ["Chuck Palahniuk", "Jim Uhls"])
-        self.assertEqual(movie.origin_country, ["US"])
-        self.assertEqual(movie.budget, 63000000)
-        self.assertEqual(movie.revenue, 100853753)
-        self.assertEqual(movie.vote_count, 12000)
-        self.assertEqual(movie.popularity, 45.0)
-        self.assertEqual(movie.poster_images, ["poster1.jpg", "poster2.jpg"])
-        self.assertEqual(movie.banner_images, ["banner1.jpg", "banner2.jpg"])
+        self.assertEqual(type(self.movie.director), list)
+        self.assertEqual(self.movie.director, ["David Fincher"])
+        self.assertEqual(type(self.movie.writer), list)
+        self.assertEqual(self.movie.writer, ["Chuck Palahniuk", "Jim Uhls"])
+        self.assertEqual(type(self.movie.origin_country), list)
+        self.assertEqual(self.movie.origin_country, ["US"])
 
-    def test_title_correct(self):
+    def test_basic_number_fields(self):
+        self.assertEqual(type(self.movie.budget), int)
+        self.assertEqual(self.movie.budget, 63000000)
+        self.assertEqual(type(self.movie.revenue), int)
+        self.assertEqual(self.movie.revenue, 100853753)
+        self.assertEqual(type(self.movie.vote_count),int)
+        self.assertEqual(self.movie.vote_count, 12000)
+        self.assertEqual(type(self.movie.popularity), float)
+        self.assertEqual(self.movie.popularity, 45.0)
+
+    def test_title_and_original_correct(self):
         """Test that the title field is saved correctly."""
         self.assertNotEqual(self.movie.title, "Inception")
         self.assertEqual(self.movie.title, "Fight Club")
         self.assertEqual(type(self.movie.title), str)
 
+        self.assertNotEqual(self.movie.original_title, "")
+        self.assertEqual(self.movie.original_title, "Fight Club")
+        self.assertEqual(type(self.movie.original_title), str)
+
     def test_tagline_field(self):
         """Test that the tagline field is saved correctly."""
-        movie = self.movie
-        self.assertEqual(type(movie.tagline), str)
-        self.assertEqual(movie.tagline, "Mischief. Mayhem. Soap.")
+        self.assertEqual(type(self.movie.tagline), str)
+        self.assertEqual(self.movie.tagline, "Mischief. Mayhem. Soap.")
 
     def test_overview_field(self):
         """Test that the description field is saved correctly."""
-        movie = self.movie
         self.assertIsNotNone(self.movie.overview)
-        self.assertEqual(type(movie.overview), str)
-        if self.movie.overview:
-            self.assertIn("insomniac and a slippery soap", self.movie.overview)
+        self.assertEqual(type(self.movie.overview), str)
+        self.assertIn("insomniac and a slippery soap", self.movie.overview) # type: ignore
 
     def test_movie_genre_field(self):
         """Test that the genre field can store a list of strings."""
         self.assertIsInstance(self.movie.genre, list)
         self.assertIsNotNone(self.movie.genre)
+        self.assertEqual(len(self.movie.genre), 2) # type: ignore
         self.assertEqual(self.movie.genre, ["Drama", "Thriller"])
-        if self.movie.genre:
-            self.assertIn("Drama", self.movie.genre)
-            self.assertIn("Thriller", self.movie.genre)
-            self.assertEqual(len(self.movie.genre), 2)
-
-    def test_movie_released_field(self):   
-        """Test that a movie can be marked as released."""
-        self.assertEqual(type(self.movie.released), bool)
-        self.assertTrue(self.movie.released)
+        self.assertIn("Thriller", self.movie.genre) # type: ignore
 
     def test_serie_status_field(self):   
         """Test that a movie can be marked as released."""
-        # movie = Movie.objects.get(pk=1)
         self.assertEqual(type(self.movie.status), str)
-        # self.assertTrue(self.serie.status)
         self.assertIsNotNone(self.movie.status)
         self.assertTrue(self.movie.status == "Released")
 
@@ -115,78 +121,56 @@ class MovieModelSaveTest(TestCase):
 
     def test_casting_field(self):
         """Test that the casting field is saved correctly."""
-        movie = self.movie
-        self.assertIsNotNone(movie.casting)
-        self.assertEqual(type(movie.casting), list)
-        if movie.casting:
-            self.assertEqual(len(movie.casting), 4)
-            self.assertEqual(type(movie.casting[0]), dict)
-            self.assertEqual(movie.casting[0]['name'], "Brad Pitt")
-            self.assertEqual(movie.casting[0]['role'], "Tyler Durden")
-            self.assertEqual(movie.casting[2]['name'], "Helena Bonham Carter")
-            self.assertEqual(movie.casting[2]['role'], "Marla Singer")
+        self.assertIsNotNone(self.movie.casting)
+        self.assertEqual(type(self.movie.casting), list)
+        if self.movie.casting:
+            self.assertEqual(len(self.movie.casting), 4)
+            self.assertEqual(type(self.movie.casting[0]), dict)
+            self.assertEqual(self.movie.casting[0]['name'], "Brad Pitt")
+            self.assertEqual(self.movie.casting[0]['role'], "Tyler Durden")
+            self.assertEqual(self.movie.casting[2]['name'], "Helena Bonham Carter")
+            self.assertEqual(self.movie.casting[2]['role'], "Marla Singer")
 
     def test_serie_production_field(self):
         """Test that the production field can store a list of strings."""
-        movie = self.movie
-        self.assertIsInstance(movie.production, list)
-        self.assertIsNotNone(movie.production)
-        if movie.production:
-            self.assertEqual(len(movie.production), 1)
-            self.assertIn("20th Century Fox", movie.production)
+        self.assertIsInstance(self.movie.production, list)
+        self.assertIsNotNone(self.movie.production)
+        if self.movie.production:
+            self.assertEqual(len(self.movie.production), 1)
+            self.assertEqual(self.movie.production[0], "20th Century Fox")
 
     def test_serie_original_language_field(self):
         """Test that the original_language field is saved correctly."""
-        movie = self.movie
-        self.assertIsNotNone(movie.original_language)
-        self.assertEqual(type(movie.original_language), str)
-        if movie.original_language:
-            self.assertEqual(movie.original_language, "en")
-            self.assertIn("en", movie.original_language)
+        self.assertIsNotNone(self.movie.original_language)
+        self.assertEqual(type(self.movie.original_language), str)
+        if self.movie.original_language:
+            self.assertEqual(self.movie.original_language, "en")
+            self.assertIn("en", self.movie.original_language)
     
     def test_serie_spoken_languages_field(self):
         """Test that the spoken_languages field can store a list of strings."""
-        movie = self.movie
-        self.assertIsNotNone(movie.spoken_languages)
-        self.assertIsInstance(movie.spoken_languages, list)
-        if movie.spoken_languages:
-            self.assertIn("en", movie.spoken_languages)
-            self.assertIn("de", movie.spoken_languages)
-            self.assertEqual(len(movie.spoken_languages), 2)
+        self.assertIsNotNone(self.movie.spoken_languages)
+        self.assertIsInstance(self.movie.spoken_languages, list)
+        if self.movie.spoken_languages:
+            self.assertIn("en", self.movie.spoken_languages)
+            self.assertIn("de", self.movie.spoken_languages)
+            self.assertEqual(len(self.movie.spoken_languages), 2)
 
     def test_vote_average_field(self):
         """Test that the vote_average field is saved correctly."""
-        movie = self.movie
-        self.assertIsNotNone(movie.vote_average)
-        self.assertEqual(type(movie.vote_average), float)
-        self.assertEqual(movie.vote_average, 8.4)
+        self.assertIsNotNone(self.movie.vote_average)
+        self.assertEqual(type(self.movie.vote_average), float)
+        self.assertEqual(self.movie.vote_average, 8.4)
 
-    # may go with Core app (but here tied to Movie model at the moment)
-    def test_slug_generated_on_save(self):
-        """Test that a slug is generated upon saving the movie."""
-        self.assertIsNotNone(self.movie.slug)
-        self.assertEqual(type(self.movie.slug), str)
-        if self.movie.slug:
-            self.assertIn("fight-club", self.movie.slug)
-            self.assertRegex(self.movie.slug, r'fight-club-[0-9]*-\w*\d')
-
-    def test_movie_create_required_fields_only(self):
-        """Test creating a movie with only the title (required field)."""
-        movie = Movie.objects.create(
-            title="Inception",
-            tmdb_id=27205
-            )
-        self.assertEqual(movie.title, "Inception")
-        self.assertIsNotNone(movie.pk)  # Has a database ID
-
-    def test_movie_tmdb_id_unique_constraint(self):
-        """
-        Test that tmdb_id must be unique.\n
-        Fail on attempting to create a new movie with an already existing tmdb_id.
-        """
-        Movie.objects.create(title="Movie 1", tmdb_id=12345)
-        with self.assertRaises(Exception):  # Should raise IntegrityError
-            Movie.objects.create(title="Movie 2", tmdb_id=12345)
+    def test_banner_images_field(self):
+        """Test that poster_images array is saved correctly."""
+        self.assertIn("banner1.jpg", self.movie.banner_images)
+        self.assertEqual(self.movie.banner_images, ["banner1.jpg", "banner2.jpg"])
+    
+    def test_poster_images_field(self):
+        """Test that poster_images array is saved correctly."""
+        self.assertIn("poster1.jpg", self.movie.poster_images)
+        self.assertEqual(self.movie.poster_images, ["poster1.jpg", "poster2.jpg"])
 
     def test_movie_trailers_field(self):
         """Test that the trailers field is saved correctly."""
@@ -198,6 +182,106 @@ class MovieModelSaveTest(TestCase):
             self.assertEqual(type(movie.trailers[0]), dict)
             self.assertEqual(movie.trailers[0]['key'], "8hP9D6kZseM")
             self.assertEqual(movie.trailers[0]['website'], "youtube")
+
+    # may go with Core app (but here tied to Movie model at the moment)
+    def test_slug_generated_on_save(self):
+        """Test that a slug is generated upon saving the movie."""
+        self.assertIsNotNone(self.movie.slug)
+        self.assertEqual(type(self.movie.slug), str)
+        if self.movie.slug:
+            self.assertIn("fight-club", self.movie.slug)
+            self.assertRegex(self.movie.slug, r'fight-club-[0-9]*-\w*\d')
+
+
+    # -------- Testing empty fields -----------------------------
+    def test_create_empty_movie(self):
+        '''
+        -- Test that Movie fail to save in db and trigger ValidationErrror as no title are given.
+        '''
+        movie = Movie(title="")
+        with self.assertRaises(ValidationError):
+            movie.full_clean()
+            movie.save()
+
+    # def test_create_empty_movie_2(self):
+    #     '''
+    #     -- Test that Movie fail to save in db and trigger ValidationErrror as no title are given.
+    #     '''
+    #     empt_movie = self.movie_empty
+    #     with self.assertRaises(ValidationError):
+    #         empt_movie.full_clean()
+    #         empt_movie.save()
+
+    def test_vote_average_accepts_none(self):
+            """-- Test that vote_average can be None (blank=True, null=True)."""
+            movie = Movie(title="untitled", vote_average=None)
+            movie.full_clean()
+            movie.save()
+            self.assertIsNone(movie.vote_average)
+
+    def test_genre_accepts_none(self):
+            """-- Test that vote_average can be None (blank=True, null=True)."""
+            movie = Movie(title="untitled", genre=None)
+            movie.full_clean()
+            movie.save()
+            self.assertIsNone(movie.genre)
+
+    # ------------- Testing validation --------------------------------
+    def test_vote_average_below_value_limit(self):
+        """-- Test that vote_average validation works. Cannot be negative"""
+        new_movie = Movie(title="Invalid", tmdb_id=55, vote_average=-3.2)
+        with self.assertRaises(ValidationError):
+            new_movie.full_clean()
+            new_movie.save()
+
+    def test_vote_average_min_edge_value_limit(self):
+        new_movie_2 = Movie(title="test film", tmdb_id=56, vote_average=0)
+        new_movie_2.full_clean()
+        new_movie_2.save()
+        
+    def test_vote_average_over_value_limit(self):
+        """-- Test that vote_average validation works. Cannot be over 10.0"""
+        new_movie = Movie(title="Invalid", tmdb_id=57, vote_average=15.0)
+        with self.assertRaises(ValidationError):
+            new_movie.full_clean()
+            new_movie.save()
+
+    def test_vote_average__max_edge_value_limit(self):
+        new_movie_2 = Movie(title="test film 2", tmdb_id=58, vote_average=10)
+        new_movie_2.full_clean()
+        new_movie_2.save()
+
+    # -------- Testing constraints -----------------------------
+    def test_movie_create_required_fields_only(self):
+        """Test creating a movie with only the title (required field)."""
+        movie = Movie.objects.create(
+            title="Inception",
+            tmdb_id=27205
+            )
+        self.assertEqual(movie.title, "Inception")
+        self.assertIsNotNone(movie.pk)  # Has a database ID
+
+    def test_movie_create_without_title(self):
+        """
+        Test creating a movie with only the title (required field).\n
+        Should fail as title is required
+        """
+        movie = Movie.objects.create(
+            tmdb_id=27211
+            )
+        with self.assertRaises(ValidationError):
+            movie.full_clean()
+            movie.save()
+
+
+    def test_movie_tmdb_id_unique_constraint(self):
+        """
+        Test that tmdb_id must be unique.\n
+        Fail on attempting to create a new movie if the tmdb_id already exist.
+        """
+        Movie.objects.create(title="Movie 1", tmdb_id=12345)
+        with self.assertRaises(IntegrityError):  # Should raise IntegrityError
+            Movie.objects.create(title="Movie 2", tmdb_id=12345)
 
 
 class MovieModelMethodTest(TestCase):
@@ -214,7 +298,6 @@ class MovieModelMethodTest(TestCase):
             director=["Christopher Nolan"],
             length=148,
             trailers=[{"key": "8hP9D6kZseM", "website": "youtube"}],
-            released=True,
             origin_country=["US"],
             vote_average=8.812,
             spoken_languages=["en", "fr"],
@@ -238,7 +321,6 @@ class MovieModelMethodTest(TestCase):
             genre=["Drama", "Thriller"],
             release_date=datetime.datetime.strptime("1999-10-15", '%Y-%m-%d').date(),
             length=139,
-            released=True,
             director=["David Fincher"],
             writer=["Chuck Palahniuk", "Jim Uhls"],
             production=["20th Century Fox"],
@@ -246,7 +328,6 @@ class MovieModelMethodTest(TestCase):
                 {"name": "Brad Pitt", "role": "Tyler Durden"},
                 {"name": "Edward Norton", "role": "The Narrator"},
                 {"name": "Helena Bonham Carter", "role": "Marla Singer"},
-                {"name": "Meat Loaf", "role": "Robert 'Bob' Paulson"}
                 ],
             origin_country=["US"],
             original_language="en",
@@ -263,27 +344,15 @@ class MovieModelMethodTest(TestCase):
             trailers=[{"key": "8hP9D6kZseM", "website": "youtube"}]
         )
 
-        print("         --SetupTestData for MovieModelMethodTest completed.\n")
-
-    # def setUp(self):
-    #     Movie.objects.create(
-    #         title="Inception",
-    #         genre=["Action", "Sci-Fi"],
-    #         casting=[{"name": "Leonardo DiCaprio", "role": "Cobb"}, {"name": "Joseph Gordon-Levitt", "role": "Arthur"}],
-    #         writer=["Christopher Nolan"],
-    #         production=["Warner Bros."],
-    #         director=["Christopher Nolan"]
-    #     )
+        print("\n\n-- SetupTestData for MovieModelMethodTest completed.\n")
 
     def test_movie_str_method_returns_title(self):
-        """Test that the __str__ method returns the movie's title."""
+        """-- Test that the __str__ method returns the movie's title."""
         self.assertEqual(str(self.movie_1), "Inception")
         self.assertEqual(str(self.movie_2), "the dark knight")
 
     def test_render_genre(self):
-        '''
-        Test the render_genre method for correct formatting.
-        '''
+        '''-- Test the render_genre method for correct formatting. '''
         # check movie 1
         self.assertEqual(self.movie_1.render_genre(), "Action - Sci-Fi")
         # check movie 2
@@ -316,7 +385,7 @@ class MovieModelMethodTest(TestCase):
 
     def test_render_banner_images(self):
         ''' 
-        test the render_banner method.
+        -- test the render_banner method.
         Should return full url: preffix + random image name from the field's list
         '''
         self.assertNotEqual(self.movie_3.render_banner(), "banner1.jpg")
@@ -328,7 +397,7 @@ class MovieModelMethodTest(TestCase):
 
     def test_render_poster_images(self):
         ''' 
-        test the render_poster method.
+        -- test the render_poster method.
         Should return full url: preffix + random image name from the field's list
         '''
         self.assertNotEqual(self.movie_3.render_poster(), "poster1.jpg")
@@ -339,4 +408,3 @@ class MovieModelMethodTest(TestCase):
         )
         # no poster image set in movie_2
         self.assertEqual(self.movie_2.render_poster(), static("images/default_poster_photo.jpg"))
-
