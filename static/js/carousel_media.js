575f2118d75f46d8cfa04a0ea399b3adaeee527e
jon11L <jonathan.lemari@gmail.com>
2026-06-24 01:42:01 +0200

    Fixed bug animation click on media_card not clearing other media transition.

--- static/js/carousel_media.js
+++ static/js/carousel_media.js
@@ -1,12 +1,11 @@
+let hoverTimer = null;
+
 document.querySelectorAll('.carousel-card .media-card').forEach(card => {
     // initialize hover timer
-    let hoverTimer = null;
 
     card.addEventListener('mouseenter', () => {
         // Cancel any pending timer from a previous card
         clearTimeout(hoverTimer);
-
-        // console.log('card being hovered')
 
         // Set a timer befor the hovered transition/animation starts
         hoverTimer = setTimeout(() => {
@@ -28,10 +27,10 @@ document.querySelectorAll('.carousel-card .media-card').forEach(card => {
             card.classList.remove('is-hovered');
         }
             else {
+                clearTimeout(hoverTimer); // cancel if moved away before 2s
                 hoverTimer = setTimeout(() => {
                     card.classList.add('is-hovered');
                 }, 1250);
-
             }
     });
 
